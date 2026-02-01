2c01108314debca39878da5b3afabf5230c8d254
kongkrit <45794368+kongkrit@users.noreply.github.com>
2026-02-01 10:53:03 +0700

    follow lectures

--- code/_lectures/301_painter/painter.c
+++ code/_lectures/301_painter/painter.c
@@ -14,8 +14,7 @@
 // UART debug output
 #define UART_DATA_OUT ((volatile uint8_t *)0xE040U)
 
-// typedefs
-
+// typedefs of enum's and struct's
 typedef enum {
     UP,
     DOWN,
@@ -24,90 +23,77 @@ typedef enum {
     TOGGLE_COLOR
 } command;
 
-// MODEL state and prototypes:
-
 typedef struct {
     uint8_t matrix[16];
     uint8_t row, col;
-    bool color; // false = black, true = yellow
+    uint8_t color; // 0 = black, 1 = yellow
 } model_t;
 
-model_t m;
-
 // MODEL functions
-void model_paint_row_col(void);
-
-void model_init(void){
-    for (uint8_t i = 0; i < MATRIX_WIDTH; i=i+1) {
-        m.matrix[i] = B8(00000000);
-    }
-    m.row = 3;
-    m.col = 4;
-    m.color = true;
-    model_paint_row_col();
-}
-
-void model_paint_row_col(void){
-    if (m.color) {
+void model_paint_row_col(model_t *mp, uint8_t row, uint8_t col, uint8_t color){ 
+    if (color) { // we only support 0=black and 1=yellow now
         // set dot, don't touch others
-        m.matrix[m.col] = m.matrix[m.col] | (1U << m.row);
+        mp->matrix[col] = mp->matrix[col] | (1U << row);
     } else {
         // clear dot, don't touch others
-        m.matrix[m.col] = m.matrix[m.col] & ~(1U << m.row);
+        mp->matrix[col] = mp->matrix[col] & ~(1U << row);
     }
 }
 
-void model_toggle_color(void){
-    m.color = !m.color;
+void model_init(model_t *mp){
+    for (uint8_t i = 0; i < MATRIX_WIDTH; i=i+1) {
+        mp->matrix[i] = B8(00000000);
+    }
+    mp->row = 3;
+    mp->col = 4;
+    mp->color = 1;
+    model_paint_row_col(mp, mp->row, mp->col, mp->color);
 }
 
-// MODEL move
-void model_update(command c) {
+// MODEL move or switch color
+void model_update(model_t *mp, command c) {
 
-    // declare new state
-    uint8_t new_row, new_col;
-    bool new_color;
-
-    // new_state = f(state, input)
-    new_row      = m.row;
-    new_col      = m.col;
-    new_color    = m.color;
+    // declare new necessary stuff
+    uint8_t new_row   = mp->row;
+    uint8_t new_col   = mp->col;
+    uint8_t new_color = mp->color;
+    // we don't copy matrix. it's not needed
 
     switch (c) {
-        case DOWN:  new_row = (m.row == 0) ? m.row : m.row - 1;
+        case DOWN:  new_row = (mp->row == 0) ? mp->row : mp->row - 1;
                     break;
-        case UP:    new_row = (m.row == 7) ? m.row : m.row + 1;
+        case UP:    new_row = (mp->row == 7) ? mp->row : mp->row + 1;
                     break;
-        case LEFT:  new_col = (m.col == 0) ? m.col : m.col - 1;
+        case LEFT:  new_col = (mp->col == 0) ? mp->col : mp->col - 1;
                     break;
-        case RIGHT: new_col = (m.col == (MATRIX_WIDTH-1)) ? m.col: m.col + 1;
+        case RIGHT: new_col = (mp->col == (MATRIX_WIDTH-1)) ? mp->col: mp->col + 1;
                     break;
         case TOGGLE_COLOR:  
-                    new_color = !m.color;
+                    new_color = !mp->color;
                     break;
         default:    break;
     }
 
     // state <= next_state
-    m.row      = new_row;
-    m.col      = new_col;
-    m.color    = new_color;
+    mp->row      = new_row;
+    mp->col      = new_col;
+    mp->color    = new_color;
     // update model
-    model_paint_row_col();
+    model_paint_row_col(mp, mp->row, mp->col, mp->color);
 }
 
 // VIEW update - transfer MODEL to VIEW
-void update_view(model_t m_copy){
+void update_view(const model_t *mp){
     for (uint8_t i = 0; i < MATRIX_WIDTH; i=i+1) {
-        *(MATRIX_BASE + i) = m_copy.matrix[i];
+        *(MATRIX_BASE + i) = mp->matrix[i];
     }
-    *(HEX_DISP +1) = m_copy.row;
-    *(HEX_DISP)    = m_copy.col;
-    *(COLOR_DISP)  = m_copy.color;
+    *(HEX_DISP +1) = mp->row;
+    *(HEX_DISP)    = mp->col;
+    *(COLOR_DISP)  = mp->color;
 }
 
 // CONTROLLER
-void read_keypad_and_model_update(void){
+void read_keypad_and_model_update(model_t *mp){
     uint8_t key = *KEYPAD;  // remember KEYPAD is read2clear
     if (key & B8(10000000)) {
         // key is valid, strip valid bit
@@ -119,28 +105,30 @@ void read_keypad_and_model_update(void){
     }
     switch (key) {
         case B8(00010001) : // row 1 col 1, UP arrow
-                            model_update(UP);
+                            model_update(mp, UP);
                             break;
         case B8(00100000) : // row 2 col 0, LEFT arrow
-                            model_update(LEFT);
+                            model_update(mp, LEFT);
                             break;
         case B8(00100010) : // row 2 col 2, RIGHT arrow
-                            model_update(RIGHT);
+                            model_update(mp, RIGHT);
                             break;
         case B8(00110001) : // row 3 col 1, DOWN arrow
-                            model_update(DOWN);
+                            model_update(mp, DOWN);
                             break;
         case B8(00110011) : // row 3 col 3, TOGGLE_COLOR
-                            model_update(TOGGLE_COLOR);
+                            model_update(mp, TOGGLE_COLOR);
                             break;
         default:            break;
     }
 }
 
 void main(void){
-    model_init();
+    model_t m;
+    model_t *mp = &m;
+    model_init(mp);
     while (true) {
-        read_keypad_and_model_update();
-        update_view(m);
+        read_keypad_and_model_update(mp);
+        update_view(mp);
     }
 }
