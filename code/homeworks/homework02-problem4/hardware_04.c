352d8fccc1c59b8f368fc7a8d368e9f13cf904f0
jumptiwa <jumptiwa@gmail.com>
2026-02-01 21:39:15 +0700

    Update hardware_04.c

--- code/homeworks/homework02-problem4/hardware_04.c
+++ code/homeworks/homework02-problem4/hardware_04.c
@@ -1,16 +1,34 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define LED0 ((volatile bool *)0xE000U)
+#define LED0 ((volatile bool *)0xE000)
+#define LED1 ((volatile bool *)0xE001)
+#define LED2 ((volatile bool *)0xE002)
+#define LED3 ((volatile bool *)0xE003)
 
-void main(void) {
+void delay(void) {
+    for (volatile int i = 0; i < 100000; i++);
+}
 
-    volatile bool *led; 
-    led = LED0;
+int main(void) {
+
+    volatile bool *led_seq[] = {
+        LED1,   // E001
+        LED2,   // E002
+        LED0,   // E000
+        LED3,   // E003
+        LED2,   // E002
+        LED0    // E000
+    };
+
+    int i = 0;
 
     while (true) {
-        *led = true;
-        *led = false;
-    }
+        *led_seq[i] = true;
+        delay();
+        *led_seq[i] = false;
+        delay();
 
+        i = (i + 1) % 6;   // loop forever
+    }
 }
