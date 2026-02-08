a112b0bc09e3d8a9146cb408e1c6d18deba3dadf
kongkrit <45794368+kongkrit@users.noreply.github.com>
2026-02-08 18:58:47 +0700

    Update dot_runner.c

--- code/_lectures/401_dot_runner/dot_runner.c
+++ code/_lectures/401_dot_runner/dot_runner.c
@@ -8,7 +8,9 @@
 // VIEW
 #define MATRIX_BASE ((volatile uint8_t * const)0xE020U)
 #define HEX_DISP ((volatile uint8_t * const)0xE808U)
-#define TICK_LED ((volatile bool *)0xE000U)
+
+// NMI handler monitor
+#define IN_NMI_LED ((volatile bool *)0xE000U)
 
 // CONTROLLER
 #define KEYPAD ((volatile const uint8_t * const)0xD010U)
@@ -37,31 +39,19 @@ void model_update(model_t *mp, command c){
     static uint8_t new_row, new_col;
     static command new_cmd;
     new_cmd = (c == NONE) ? mp->cmd : c;
+    new_row = mp->row;
+    new_col = mp->col;
     switch (new_cmd){
-        case STOP:  new_row = mp->row;
-                    new_col = mp->col;
-                    new_cmd = STOP;
+        case STOP:  break;
+        case LEFT:  new_col = (mp->col == 0)? DISP_WIDTH -1 : mp->col - 1;
                     break;
-        case LEFT:  new_row = mp->row;
-                    new_col = (mp->col == 0)? DISP_WIDTH -1 : mp->col - 1;
-                    new_cmd = LEFT;
+        case RIGHT: new_col = (mp->col >= DISP_WIDTH -1)? 0 : mp->col + 1;
                     break;
-        case RIGHT: new_row = mp->row;
-                    new_col = (mp->col >= DISP_WIDTH -1)? 0 : mp->col + 1;
-                    new_cmd = RIGHT;
+        case DOWN:  new_row = (mp->row == 7)? 0 : mp->row + 1;
                     break;
-        case DOWN:  new_col = mp->col;
-                    new_row = (mp->row == 7)? 0 : mp->row + 1;
-                    new_cmd = DOWN;
+        case UP:    new_row = (mp->row == 0)? 7 : mp->row - 1;
                     break;
-        case UP:    new_col = mp->col;
-                    new_row = (mp->row == 0)? 7 : mp->row - 1;
-                    new_cmd = UP;
-                    break;
-        default:    new_col = mp->col;
-                    new_row = mp->row;
-                    new_cmd = mp->cmd;
-                    break;
+        default:    break;
     }
     // update state
     mp->cmd = new_cmd;
@@ -82,30 +72,29 @@ command controller_read(void){
     static uint8_t key;
     static command c;
     key = *KEYPAD;
-    if (key & B8(10000000)){
-        // valid key
-        switch (key & B8(01111111)){
-            case B8(00010001):  c = UP;     // UP
-                                break;
-            case B8(00100000):  c = LEFT;   // LEFT
-                                break;
-            case B8(00100001):  c = STOP;   // STOP
-                                break;
-            case B8(00100010):  c = RIGHT;  // RIGHT
-                                break;
-            case B8(00110001):  c = DOWN;   // DOWN
-                                break;
-            default:            c = NONE;
-                                break; 
-        }
-    } else {
-        c = NONE;
+    if (! (key & B8(10000000)) ){
+        return NONE;    // no new keypress
+    }
+    // valid key
+    switch (key & B8(01111111)){
+        case B8(00010001):  c = UP;     // UP
+                            break;
+        case B8(00100000):  c = LEFT;   // LEFT
+                            break;
+        case B8(00100001):  c = STOP;   // STOP
+                            break;
+        case B8(00100010):  c = RIGHT;  // RIGHT
+                            break;
+        case B8(00110001):  c = DOWN;   // DOWN
+                            break;
+        default:            c = NONE;
+                            break; 
     }
     return c;
 }
 
 void nmi_handler(void) __critical __interrupt {
-    // we declare them all as static to avoid paying stack allocation
+    // we declare them all as static to avoid stack allocation
     static bool busy = false;
     static bool initialized = false;
     static model_t m;
@@ -117,10 +106,12 @@ void nmi_handler(void) __critical __interrupt {
     }
 
     busy = true;
+    *IN_NMI_LED = true;
     if (!initialized) {
         model_init(mp);
         initialized = true;
         busy = false;
+        *IN_NMI_LED = false;
         return;
     }
     // CONTROLLER read and MODEL update
@@ -128,6 +119,7 @@ void nmi_handler(void) __critical __interrupt {
     model_update(mp, c);
     view_update(mp);
     busy = false;
+    *IN_NMI_LED = false;
     return;
 }
 
